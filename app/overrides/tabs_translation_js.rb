410726c536b4b833ecb13567bd0d7f7816a0fc98
Pavel Brylov <pavelb@microsoft.com>
2012-04-09 01:48:32 +0300

    Use generic name for override so other extensions may use same override name and it won't be duplicated

--- app/overrides/tabs_translation_js.rb
+++ app/overrides/tabs_translation_js.rb
@@ -1,5 +1,5 @@
 Deface::Override.new(
   :virtual_path => "spree/layouts/admin",
-  :name => 'product_translation_js',
+  :name => 'tabs_translation_js',
   :insert_bottom => "[data-hook='admin_inside_head']",
   :text => %Q[<%= javascript_tag "$(function(){ $('.tabs').livequery(function() { $(this).tabs(); }); });" %>])
